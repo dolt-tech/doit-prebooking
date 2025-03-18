f7e03fd1bd803c00a03fc60371a1df930f452a97
dolt-tech <test.d0lt.com@gmail.com>
2025-03-18 12:36:22 +0530

    first commit

---
+++ Backend/Models/Homeowner.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const HomeownerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  mobnumber: { type: String, required: true, unique: true }, 
+  role: { type: String, enum: ["homeowner"], default: "homeowner" },
+});
+
+const Homeowner = mongoose.model("Homeowner", HomeownerSchema);
+module.exports = Homeowner;
